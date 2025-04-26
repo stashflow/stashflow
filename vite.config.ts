f1ef0f241e2646848abf77f408bdebd900a48b73
stashflow <bartholomewiscool99@gmail.com>
2025-04-26 18:07:43 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,17 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
